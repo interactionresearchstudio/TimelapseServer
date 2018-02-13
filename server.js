499b39ab1415ceb5ae1c730954057a89d9a5cced
Mike Vanis <hello@mikevanis.com>
2018-02-13 14:57:41 +0000

    v0.1.0

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const fileUpload = require('express-fileupload');
 const randomstring = require('randomstring');
 const videoshow = require('videoshow');
 const fs = require('fs');
+const async = require('async');
 const http = require('http');
 const app = express();
 
@@ -13,9 +14,52 @@ app.use(fileUpload());
 
 var uploadPath = 'public/uploads/';
 
+function downloadMP4(directory, outputName, duration, resultObject) {
+    fs.readdir(directory, function(err, files) {
+        for(var i=0; i<files.length; i++) {
+            files[i] = directory + '/' + files[i];
+            console.log('INFO - Item in directory: ' + files[i]);
+        }
+        var videoOptions = {
+            fps: 25,
+            loop: duration, // seconds
+            transition: false,
+            transitionDuration: 1, // seconds
+            videoBitrate: 1024,
+            videoCodec: 'libx264',
+            size: '1920x?',
+            audioBitrate: '128k',
+            audioChannels: 2,
+            format: 'mp4',
+            pixelFormat: 'yuv420p'
+        }
+        videoshow(files, videoOptions)
+            .save(directory + '/' + outputName + '.mp4')
+            .on('start', function(command) {
+                console.log('INFO - ffmpeg process started.')
+            })
+            .on('error', function (err, stdout, stderr) {
+                console.error('Error:', err)
+                console.error('ffmpeg stderr:', stderr)
+                return '';
+            })
+            .on('end', function (output) {
+                console.log('INFO - Video created in:', output)
+                resultObject.download(directory + '/' + outputName + '.mp4');
+                return directory + '/' + outputName + '.mp4';
+            })
+    });
+}
+
+function compileGIF(directory, outputName, duration) {
+
+}
+
 // Post
 app.post('/uploadFiles', function(req, res) {
     if (!req.files) return res.status(400).send('No files were uploaded.');
+
+    console.log('INFO - Output type: ' + req.body.outputType);
 
     var numOfFilesUploaded = 0;
 
@@ -41,39 +85,9 @@ app.post('/uploadFiles', function(req, res) {
         console.log("INFO - Saved " + req.files.uploadedImages[i].name + " to public folder.");
     }
 
-    fs.readdir(uploadPath + currentFolderName, function(err, files) {
-        for(var i=0; i<files.length; i++) {
-            files[i] = uploadPath + currentFolderName + '/' + files[i];
-            console.log('INFO - Item in directory: ' + files[i]);
-        }
-        var videoOptions = {
-            fps: 25,
-            loop: 0.3, // seconds
-            transition: false,
-            transitionDuration: 1, // seconds
-            videoBitrate: 1024,
-            videoCodec: 'libx264',
-            size: '1920x?',
-            audioBitrate: '128k',
-            audioChannels: 2,
-            format: 'mp4',
-            pixelFormat: 'yuv420p'
-        }
-        videoshow(files, videoOptions)
-            .save(uploadPath + currentFolderName + '/' + currentFolderName + '.mp4')
-            .on('start', function(command) {
-                console.log('INFO - ffmpeg process started.')
-            })
-            .on('error', function (err, stdout, stderr) {
-                console.error('Error:', err)
-                console.error('ffmpeg stderr:', stderr)
-            })
-            .on('end', function (output) {
-                console.log('INFO - Video created in:', output)
-            })
-    });
-
-    res.send(numOfFilesUploaded + " files uploaded!");
+    // Download file
+    if (req.body.outputType == 'mp4')
+        downloadMP4(uploadPath + currentFolderName, currentFolderName, parseFloat(req.body.frameDelay), res);
 });
 
 http.createServer(app).listen(app.get('port'), function() {
