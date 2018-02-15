9fe314695c7e61e5ba63aaeb87abbc21e4a68f8a
Mike Vanis <hello@mikevanis.com>
2018-02-15 12:59:33 +0000

    dockerised

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const http = require('http');
 const app = express();
 
 // App setup
-app.set('port', process.env.PORT || 5000);
+app.set('port', process.env.PORT || 8080);
 app.use(express.static(__dirname + '/public'));
 app.use(fileUpload());
 
